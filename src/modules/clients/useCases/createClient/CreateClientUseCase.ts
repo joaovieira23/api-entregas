f4999621cb83162896307de49ddcd54f74c6af3c
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-09 22:29:48 -0300

    feat: customer registration

---
+++ src/modules/clients/useCases/createClient/CreateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcrypt";
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateClient {
+    username: string;
+    password: string;
+}
+
+export class CreateClientUseCase {
+    async execute({ password, username } : ICreateClient) {
+        // Validar se o usuário existe
+        const clientExist = await prisma.clients.findFirst({
+            where: {
+                username: {
+                    mode: "insensitive"
+                }
+            }
+        });
+
+        if(clientExist) {
+            throw new Error("Client already exists")
+        }
+
+        // Criptografar a senha
+        const hashPassword = await hash(password, 10);
+
+        // Salvar o client
+        const client = await prisma.clients.create({
+            data: {
+                username,
+                password: hashPassword
+            }
+        });
+
+        return client;
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Hello world",
-    })
-})
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, () => console.log("Server is running"));
