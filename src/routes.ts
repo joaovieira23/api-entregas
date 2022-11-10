483da10392f76223352edb8980191a2606b00135
João Andrade <joaovictorvieira.23@hotmail.com>
2022-11-10 14:21:22 -0300

    feat: delivery register

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
@@ -11,11 +12,15 @@ const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController();
 const createDeliverymanController = new CreateDeliverymanController();
 
+const deliveryController = new CreateDeliveryController();
+
 routes.post("/client/authenticate/", authenticateClientController.handle);
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle);
 
 routes.post("/client/", createClientController.handle);
 
 routes.post("/deliveryman", createDeliverymanController.handle);
+
+routes.post('/delivery', deliveryController.handle);
 
 export { routes };
